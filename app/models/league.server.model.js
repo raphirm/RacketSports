207cbf75b31257760b190c9d8db6d572aa0b66a4
Raphael Marques <raphael@marques.com>
2015-05-14 15:38:14 +0200

    add graphics

--- app/models/league.server.model.js
+++ app/models/league.server.model.js
@@ -21,6 +21,47 @@ var LeagueSchema = new Schema({
 		default: '',
 		required: 'Please fill out the sport type'
 	},
+    ort: {
+        type: String,
+        default: 'Schweiz',
+        required: 'Please fill out the Area where your leage is built'
+    },
+    description: {
+        type: escapeHtml,
+        default: 'Neue Liga'
+    },
+    niveau: {
+        type: String,
+        default: 'Voll Schlecht',
+        required: 'Please fill out what skill level is required'
+    },
+    gernder: {
+        type: String,
+        default: 'mixed',
+        required: 'Please fill out what gender are allowed to participate'
+    },
+    mode: {
+        type: String,
+        default: 'single',
+        required: 'Please fill out what mode'
+    },
+    price: {
+        type: String,
+        default: 'none'
+    },
+    startdate: {
+        type: Date,
+        default: Date.now
+    },
+    enddate: {
+        type: Date,
+        default: date.now
+    },
+    rules: {
+        type: String,
+        default: "Ein Match alle zwei Wochen, autogeneriert, win 3 Punkte, unentschieden 1 Punkt, verlieren: 0 Punkte"
+    },
+    
 	created: {
 		type: Date,
 		default: Date.now

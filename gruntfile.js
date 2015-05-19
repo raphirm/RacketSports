618dfb8daa27f2125298a44ac77d3863319f08f5
rm <raphael@marques.com>
2015-05-19 13:09:40 +0200

    add prod env for nodemon, without debug

--- gruntfile.js
+++ gruntfile.js
@@ -32,7 +32,7 @@ module.exports = function(grunt) {
 			clientViews: {
 				files: watchFiles.clientViews,
 				options: {
-					livereload: true,
+					livereload: true
 				}
 			},
 			clientJS: {
@@ -60,7 +60,7 @@ module.exports = function(grunt) {
 		},
 		csslint: {
 			options: {
-				csslintrc: '.csslintrc',
+				csslintrc: '.csslintrc'
 			},
 			all: {
 				src: watchFiles.clientCSS
